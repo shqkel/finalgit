fcaf3c2b55f092dbb409f968b97d2ea289dd40d8
shqkel <shqkel1863@gmail.com>
2019-09-10 16:51:44 +0900

    팀장 shqkel 작업시작!!

--- 04_FinalGitTest/src/main/java/com/kh/spring/FinalGitTest.java
+++ 04_FinalGitTest/src/main/java/com/kh/spring/FinalGitTest.java
@@ -5,5 +5,6 @@ public class FinalGitTest {
 	public void test() {
 		System.out.println("팀장이 프로젝트 시작!!!");
 		System.out.println("팀원 khstudy가 작업을 시작했습니다.!!!!");
+		System.out.println("팀장 shqkel도 작업을 시작!!!");
 	}
 }
